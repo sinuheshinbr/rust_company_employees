f4bd0cef5902a325c098b4ef29cab993c29a81d8
sinuheshinbr <sinuheshin@gmail.com>
2023-11-20 12:57:17 -0300

    feat: List employees working

--- src/fns.rs
+++ src/fns.rs
@@ -1,9 +1,13 @@
+use dialoguer::{theme::ColorfulTheme, Select};
 use std::{
     collections::{hash_map::Entry, HashMap},
     io,
 };
 
-pub fn add_empoyee(map: &mut HashMap<String, Vec<String>>) {
+pub fn add_empoyee(
+    map: &mut HashMap<String, Vec<String>>,
+    on_end: fn(map: &mut HashMap<String, Vec<String>>) -> (),
+) {
     let mut command = String::new();
 
     println!("Insert the name of the employee and the department in the following format:");
@@ -11,7 +15,7 @@ pub fn add_empoyee(map: &mut HashMap<String, Vec<String>>) {
 
     match io::stdin().read_line(&mut command) {
         Ok(_) => (),
-        Err(_) => add_empoyee(map),
+        Err(_) => add_empoyee(map, on_end),
     }
 
     let words: Vec<String> = command.split_whitespace().map(|s| s.to_string()).collect();
@@ -20,20 +24,74 @@ pub fn add_empoyee(map: &mut HashMap<String, Vec<String>>) {
 
     let department = words[3].clone();
 
-    match map.entry(department) {
+    match map.entry(department.clone()) {
         Entry::Vacant(e) => {
-            e.insert(vec![name]);
+            e.insert(vec![name.clone()]);
         }
         Entry::Occupied(mut e) => {
-            e.get_mut().push(name);
+            e.get_mut().push(name.clone());
         }
     }
+
+    println!("Employee {} added to {} with success!", name, department);
+    on_end(map);
 }
 
 pub fn exit() {
     std::process::exit(exitcode::OK);
 }
 
+pub fn list_employees(
+    map: &mut HashMap<String, Vec<String>>,
+    on_end: fn(map: &mut HashMap<String, Vec<String>>) -> (),
+) {
+    let departments: Vec<String> = map.keys().map(|s| s.to_string()).collect();
+
+    if departments.len() < 1 {
+        println!("There are no employees at your company. Register someone first!");
+        return main_loop(map);
+    }
+
+    let department = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select the department")
+        .default(0)
+        .items(&departments[..])
+        .interact()
+        .unwrap();
+
+    let department = departments[department].clone();
+
+    match map.get(&department) {
+        Some(val) => println!("Empolyees at {} are: {:?}", department, val.join(", ")),
+        None => exit(),
+    }
+
+    on_end(map)
+}
+
 pub fn remove_employee() {
     println!("Removing empolyee")
+}
+
+pub fn main_loop(map: &mut HashMap<String, Vec<String>>) {
+    let actions = [
+        "Insert new employee",
+        "Remove employee",
+        "List employees",
+        "Exit",
+    ];
+    let action = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("What do you want to do?")
+        .default(0)
+        .items(&actions[..])
+        .interact()
+        .unwrap();
+
+    match actions[action] {
+        "Insert new employee" => add_empoyee(map, main_loop),
+        "Remove employee" => remove_employee(),
+        "List employees" => list_employees(map, main_loop),
+        "Exit" => exit(),
+        _ => unreachable!(),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 mod fns;
-use crate::fns::{add_empoyee, exit, remove_employee};
-use dialoguer::{theme::ColorfulTheme, Select};
+use crate::fns::main_loop;
 use std::collections::HashMap;
 
 fn main() {
     println!("Company HR mgmt program");
 
-    let actions = ["Insert new employee", "Remove employee", "Exit"];
-
     let mut map = HashMap::new();
 
-    let action = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("What do you want to do?")
-        .default(0)
-        .items(&actions[..])
-        .interact()
-        .unwrap();
-
-    match actions[action] {
-        "Insert new employee" => add_empoyee(&mut map),
-        "Remove employee" => remove_employee(),
-        "Exit" => exit(),
-        _ => unreachable!(),
-    }
+    main_loop(&mut map);
 
     println!("Actual map: {:?}", map)
 }
